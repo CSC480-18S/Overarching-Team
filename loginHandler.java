a12082c17b06e87dd597330411f8e18e7a9267a8
iblatnick <iblatnic@oswego.edu>
2018-03-29 15:00:32 -0400

    Add files via upload

---
+++ loginHandler.java
@@ -0,0 +1,106 @@
+package org.doublecloud.sample.junit.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class exchangeHandlerTest {
+
+@BeforeClass
+public static void beforeClass(){
+	String userName;
+	String password;
+	EventHandler e = new EventHandler();
+}
+
+@AfterClass
+public static void afterClass(){
+	
+}
+
+@Before
+public void setUp(){
+	
+	userName = "untaken username";
+	password = "password";
+}
+
+@After
+public void tearDown() {
+	
+	userName = "";
+	password = "";
+}
+
+public void setUp2(){
+	
+	userName = "taken username";
+	password = "password";
+}
+
+public void setUp3(){
+	
+	userName = "fuck";
+	password = "password";
+}
+
+public void setUp4() {
+	userName = "";
+	password = "password";
+}
+
+public void setUp5(){
+	userName = "untaken username"
+	password = "";
+}
+
+
+
+
+@Test
+public void exchangeHandlerEQ1(){
+	setUp();
+	int result = exchangeHandler(userName);
+	assertEquals("loginHandler username: " + username + " password: " + pass, result);
+	tearDown();
+}
+
+@Test
+public void exchangeHandlerEQ2(){
+	setUp2();
+	int result = exchangeHandler(userName);
+	assertEquals("loginHandler username: " + username + " password: " + pass, result);
+	tearDown();
+}
+
+public void exchangeHandlerEQ3(){
+	setUp3();
+	int result = exchangeHandler(userName);
+	assertEquals("loginHandler username: " + username + " password: " + pass, result);
+	tearDown();
+}
+
+public void exchangeHandlerEQ4(){
+	setUp3();
+	int result = exchangeHandler(userName);
+	assertEquals("loginHandler username: " + username + " password: " + pass, result);
+	tearDown();
+}
+
+
+public void exchangeHandlerEQ5(){
+	setUp3();
+	int result = exchangeHandler(userName);
+	assertEquals("loginHandler username: " + username + " password: " + pass, result);
+	tearDown();
+}
+
+
+
+
+
+}
---
+++ passHandler.java
@@ -0,0 +1,76 @@
+package org.doublecloud.sample.junit.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class passHandlerTest {
+
+@BeforeClass
+public static void beforeClass(){
+	String username;
+	EventHandler e = new EventHandler();
+}
+
+@AfterClass
+public static void afterClass(){
+	
+}
+
+@Before
+public void setUp(){
+	
+	username = "Scrabblefiend";
+}
+
+@After
+public void tearDown() {
+	
+	username = "";
+}
+
+public void setUp2(){
+	
+	username = "$#%^%$";
+}
+
+public void setUp3(){
+	
+	username = "";
+}
+
+
+
+
+
+@Test
+public void passHandlerEQ1(){
+	setUp();
+	String result = e.passHandler(username);
+	assertEquals("passHandler username: Scrabblefiend", result);
+	tearDown();
+}
+
+@Test
+public void passHandlerEQ2(){
+	setUp2();
+	String result = e.passHandler(username);
+	assertEquals("passHandler username: $#%^%$", result);
+	tearDown();
+}
+
+public void passHandlerEQ3(){
+	setUp3();
+	String result = e.passHandler(username);
+	assertEquals("passHandler username: ", result);
+	tearDown();
+}
+
+
+
+
+}
