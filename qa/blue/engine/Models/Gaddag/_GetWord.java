79fd1fb208f1e3c31922a6c15dc3285bbaab01b6
iblatnick <iblatnic@oswego.edu>
2018-03-29 15:42:36 -0400

    Add files via upload

---
+++ qa/blue/engine/Models/Gaddag/_GetWord.java
@@ -0,0 +1,76 @@
+package org.doublecloud.sample.junit.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class _GetWordTest {
+
+@BeforeClass
+public static void beforeClass(){
+	String word;
+	Gaddag g = new Gaddag('d');
+}
+
+@AfterClass
+public static void afterClass(){
+	
+}
+
+@Before
+public void setUp(){
+	
+	word = "dog";
+}
+
+@After
+public void tearDown() {
+	
+	word = "";
+}
+
+public void setUp2(){
+	
+	word = "ddvchh";
+}
+
+public void setUp3(){
+	
+	word = "";
+}
+
+
+
+
+
+@Test
+public void isDictionaryWordTestEQ1(){
+	setUp();
+	int result = g.isDictionaryWord(word);
+	assertEquals(1, result);
+	tearDown();
+}
+
+@Test
+public void isDictionaryWordTestEQ2(){
+	setUp2();
+	int result = g.isDictionaryWord(word);
+	assertEquals(0, result);
+	tearDown();
+}
+
+public void isDictionaryWordTestEQ3(){
+	setUp3();
+	int result = g.isDictionaryWord(word);
+	assertEquals(0, result);
+	tearDown();
+}
+
+
+
+
+}
---
+++ qa/blue/engine/Models/Gaddag/convertToArray.java
@@ -0,0 +1,76 @@
+package org.doublecloud.sample.junit.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class convertToArrayTest {
+
+@BeforeClass
+public static void beforeClass(){
+	String word;
+	Gaddag g = new Gaddag('d');
+}
+
+@AfterClass
+public static void afterClass(){
+	
+}
+
+@Before
+public void setUp(){
+	
+	word = "dog";
+}
+
+@After
+public void tearDown() {
+	
+	word = "";
+}
+
+public void setUp2(){
+	
+	word = "ddfsc";
+}
+
+public void setUp3(){
+	
+	word = "";
+}
+
+
+
+
+
+@Test
+public void convertToArrayTestEQ1(){
+	setUp();
+	int result = g.convertToArray(word);
+	assertEquals(['d','o','g'], result);
+	tearDown();
+}
+
+@Test
+public void convertToArrayTestEQ2(){
+	setUp2();
+	int result = g.convertToArray(word);
+	assertEquals(['d','d','f','s','c'], result);
+	tearDown();
+}
+
+public void convertToArrayTestEQ3(){
+	setUp3();
+	int result = g.convertToArray(word);
+	assertEquals([''], result);
+	tearDown();
+}
+
+
+
+
+}
